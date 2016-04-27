72874444a6eb26048abb4685236134371455edd2
Brandt <stargateatlantis@hotmail.dk>
2016-04-27 13:24:43 +0200

    Be Customer

--- app/src/main/java/com/example/brandt/krobookingapp/BE/Company.java
+++ app/src/main/java/com/example/brandt/krobookingapp/BE/Company.java
@@ -4,4 +4,10 @@ package com.example.brandt.krobookingapp.BE;
  * Created by kenne on 27-04-2016.
  */
 public class Company {
+    String name;
+    String phoneNumber;
+    Boolean keybox;
+    String notes;
+    String customerEmail;
+    
 }
