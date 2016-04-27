9803acdf1996029c87d0d34208643c2c350fe899
Frederik <fred5277@easv365.dk>
2016-04-27 13:21:15 +0200

    Rooms oprettet variabler

--- app/src/main/java/com/example/brandt/krobookingapp/BE/Rooms.java
+++ app/src/main/java/com/example/brandt/krobookingapp/BE/Rooms.java
@@ -1,7 +1,19 @@
 package com.example.brandt.krobookingapp.BE;
 
+import java.util.Date;
+
 /**
  * Created by kenne on 27-04-2016.
  */
 public class Rooms {
+
+    int roomNumber;
+    boolean rented;
+    boolean doubleRoom;
+    boolean bathroom;
+    int roomPrice;
+    boolean animal;
+    Date arrivalDate;
+    Date departureDate;
+    
 }
