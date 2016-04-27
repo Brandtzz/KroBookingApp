e5e5bf52c8ae221a6c450ba50b40ed98f9108826
Kenneth Jørgensen <Kenneth Jrgensen>
2016-04-27 13:24:43 +0200

    be Custemor

--- app/src/main/java/com/example/brandt/krobookingapp/BE/Custemor.java
+++ app/src/main/java/com/example/brandt/krobookingapp/BE/Custemor.java
@@ -4,4 +4,10 @@ package com.example.brandt.krobookingapp.BE;
  * Created by kenne on 27-04-2016.
  */
 public class Custemor {
+    String name;
+    String phoneNumber;
+    boolean keybox;
+    String notes;
+    String custemorEmail;
+
 }
