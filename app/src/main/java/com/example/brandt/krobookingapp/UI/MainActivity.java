fceec36c5903ad5cad5924d8fd276c601d9c9e48
Brandt <stargateatlantis@hotmail.dk>
2016-05-02 11:05:05 +0200

    Design start

--- app/src/main/java/com/example/brandt/krobookingapp/UI/MainActivity.java
+++ app/src/main/java/com/example/brandt/krobookingapp/UI/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     public void populateView(ArrayList<Rooms> all)
     {
         Log.d("XYZ", "GUI viser " + all.size() + " rum");
-        
+
     }
 
 
